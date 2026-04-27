c97d4d54ce3ee1573b4eff36110f62ee44c94d5d
Oscar Quiloulou <dimitri.acteur@outlook.com>
2026-04-27 16:22:44 +0200

    Add function to load rules from a URL

--- client/config_loader.py
+++ client/config_loader.py
@@ -1,1 +1,14 @@
+import requests
+import json
 
+def load_rules(url):
+    try:
+        r = requests.get(url, timeout=5)
+        return r.json()
+    except:
+        return {
+            "work_periods": [],
+            "blacklist": [],
+            "warning_minutes": 10,
+            "messages": {}
+        }
