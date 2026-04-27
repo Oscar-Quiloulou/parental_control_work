dfccb9f64c5c590430f3ac7665e22a177b43607f
Oscar Quiloulou <dimitri.acteur@outlook.com>
2026-04-27 16:23:05 +0200

    Implement process termination based on blacklist
    Add process termination rules based on a blacklist during work hours.

--- client/process_watcher.py
+++ client/process_watcher.py
@@ -1,1 +1,16 @@
+import psutil
+from utils import is_work_time
 
+def enforce_rules(rules):
+    if not is_work_time(rules["work_periods"]):
+        return
+
+    blacklist = [p.lower() for p in rules["blacklist"]]
+
+    for proc in psutil.process_iter(["name"]):
+        try:
+            name = proc.info["name"].lower()
+            if name in blacklist:
+                proc.terminate()
+        except:
+            pass
